4cd4dc595b02f61b8b6b6f6917f692acb3f2b84a
LexOestmann <87912594+LexOestmann@users.noreply.github.com>
2021-10-09 22:05:59 -0600

    first commit

---
+++ tttmain.js
@@ -0,0 +1,21 @@
+import Game from "./tttgame.js";
+import GameView from "./tttview.js";
+
+let game = new Game();
+let gameView = new GameView(document.getElementById("app"));
+
+// define functions
+
+gameView.onTileClick = function (i) {
+    game.makeMove(i);
+    gameView.update(game);
+
+};
+
+gameView.onRestartClick = function() {
+    game = new Game();
+    gameView.update(game);
+
+};
+
+gameView.update(game);
